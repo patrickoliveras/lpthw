3e42243f3e5ac43a13118cafb7f86f67b1ce7d63
Patrick Oliveras <patrickoliveras@jarabesoft.com>
2018-09-14 10:28:15 -0500

    Add: ex3

---
+++ ex3.py
@@ -0,0 +1,28 @@
+print("I will now count my chickens:")
+
+# Divides 30 by 6 then adds 25
+print("Hens", 25.0 + 30 / 6)
+# Multiplies 25 * 3, calculates the modulus 4 of this, then subtracts this from 100
+print("Roosters", 100.0 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+# Adds 3, 2, 1, 4 and -5, calculates the modulus 2 minus 1 of this, then divides it by 4 plus 6
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+# Adds 3 + 2 and checks if 5 - 7 is less than it, then outputs true or false accordingly
+print(3.0 + 2 < 5 - 7)
+# Prints a question concatenated, on the same line, with 3 + 2
+print("What is 3 + 2?", 3 + 2)
+# Prints a question concatenated, on the same line, with 5 + 7
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+# Prints a question, concatenating it with a comparison "is 5 greater than -2", which results in true or false
+print("Is it greater?", 5 > -2)
+# Prints a question, concatenating it with a comparison "is 5 greater or equal than -2", which results in true or false
+print("Is it greater or equal?", 5 >= -2)
+# Prints a question, concatenating it with a comparison "is 5 less or equal than -2", which results in true or false
+print("Is it less or equal?", 5 <= -2)
---
+++ ex3a.py
@@ -0,0 +1,6 @@
+print("Calculating the determinant of:")
+
+print("⌈0 1⌉")
+print("⌊1 1⌋")
+
+print("It is:", 0 * 1 - 1 * 1)
