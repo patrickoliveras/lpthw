54665c6398a72a43cf8de133911c2332e65a881b
Patrick Oliveras <patrickoliveras@jarabesoft.com>
2018-09-14 12:09:32 -0500

    Add: ex9

---
+++ ex9.py
@@ -0,0 +1,19 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# List the days of the week separated by blanks
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# List some months of the year separated by new lines
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print the days
+print("Here are the days: ", days)
+# Print the months
+print("Here are the months: ", months)
+
+# Print a paragraph using triple quotes
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
