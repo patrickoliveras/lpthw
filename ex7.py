ca1b682987826b77b6e430feb3dd164f081de9d5
Patrick Oliveras <patrickoliveras@jarabesoft.com>
2018-09-14 11:30:43 -0500

    Add: ex7

---
+++ ex7.py
@@ -0,0 +1,40 @@
+# Print first verse of the song
+print("Mary had a little lamb.")
+# Print second verse, but evaluating the color of the lamb separately
+print("Its fleece was white as {}.".format('snow'))
+# Third verse
+print("And everywhere that Mary went.")
+# Make a separator using 10 dots.
+print("." * 10) # what'd that do?
+
+# First letter of deliciousness
+end1 = "C"
+# Second letter of deliciousness
+end2 = "h"
+# Third letter of deliciousness
+end3 = "e"
+# Fourth letter of deliciousness
+end4 = "e"
+# Fifth letter of deliciousness
+end5 = "s"
+# Sixth letter of deliciousness
+end6 = "e"
+# Seventh letter of deliciousness
+end7 = "B"
+# Eighth letter of deliciousness
+end8 = "u"
+# Ninth letter of deliciousness
+end9 = "r"
+# Tenth letter of deliciousness
+end10 = "g"
+# Eleventh letter of deliciousness
+end11 = "e"
+# Twelfth letter of deliciousness
+end12 = "r"
+
+# watch end = ' ' at the end.  try removing to see what happens
+# removing the ending argument defaults to a line jump
+# Print Cheese
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# Print Burger
+print(end7 + end8 + end9 + end10 + end11 + end12)
